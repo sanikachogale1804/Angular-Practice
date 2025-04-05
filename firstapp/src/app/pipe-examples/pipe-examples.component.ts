bec4be3c3063839aba7e556d59d367bed732769c
unknown <chogalesanika8@gmail.com>
2025-04-05 09:02:30 +0530

    initial

--- firstapp/src/app/pipe-examples/pipe-examples.component.ts
+++ firstapp/src/app/pipe-examples/pipe-examples.component.ts
@@ -1,15 +1,17 @@
-import { UpperCasePipe } from '@angular/common';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pipe-examples',
-  imports: [UpperCasePipe],
+  imports: [UpperCasePipe,LowerCasePipe,TitleCasePipe,DatePipe],
   templateUrl: './pipe-examples.component.html',
   styleUrl: './pipe-examples.component.css'
 })
 export class PipeExamplesComponent {
 
   name:string="abc"
-  
+  lastname:string="SHELAR"
+  currentDate=new Date();
 
 }
